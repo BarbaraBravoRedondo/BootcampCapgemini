b2c18925f706f55fb7fb983f6cd01a2defb1bd2d
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2025-03-07 09:17:17 +0100

    Create new project "Catalogo"

--- demos/src/main/java/com/example/DemosApplication.java
+++ demos/src/main/java/com/example/DemosApplication.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemosApplication {
+public class DemosApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemosApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+        System.err.println("La aplicación ha arrancado");
+    }
 }
