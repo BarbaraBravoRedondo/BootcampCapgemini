aa2a1da9befdea0aed612b5f457fdf8302f36a5c
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2025-03-13 18:35:16 +0100

    entities/service/domains/core/repos build

--- demos/src/main/java/com/example/DemosApplication.java
+++ demos/src/main/java/com/example/DemosApplication.java
@@ -9,11 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.example.domains.contracts.repositories.ActoresRepository;
+import com.example.domains.contracts.services.ActoresService;
+import com.example.domains.entities.Actor;
 import com.example.ioc.Configuracion;
 import com.example.ioc.Rango;
 import com.example.ioc.Repositorio;
 import com.example.ioc.Servicio;
 import com.example.util.Calculadora;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.example.ioc")
@@ -21,6 +26,7 @@ public class DemosApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemosApplication.class, args);
+	
 		
 	}
 	
@@ -40,8 +46,37 @@ public class DemosApplication implements CommandLineRunner {
 	String valor;
 	@Autowired
 	Rango rango;
+	@Autowired
+	private ActoresService srv;
+//	private ActoresRepository dao;
+	private void ejemplosDatos() {
+		//var actor = new Actor(0,"Pepito","Grillo");
+//		var item =dao.findById(204);
+//		if(item.isPresent()) {
+//			var actor=item.get();
+//			actor.setFirstName("Lucas");
+//			actor.setLastName(actor.getLastName().toUpperCase());
+//			dao.save(actor);
+//		}else {System.err.println("No se han encontrado el actor");}
+////		System.err.println("Actor:" + item);
+////				//dao.save(actor);
+		//dao.findAll().forEach(System.err::println);");
+//		dao.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.err::println);
+//		dao.findAll((root, query, builder) -> builder.lessThanOrEqualTo(root.get("actorId"), 5))
+//		   .forEach(System.err::println);
+		srv.getAll().forEach(System.err::println);
+		var item =srv.getOne(1);
+		if(item.isPresent()) {
+			var actor =item.get();
+		System.err.println(item +"\nPeliculas");
+		//actor.getFilmActors().forEach(fa -> System.err.println(fa.getFilm().getTitle()));
+		}
+		else {
+		System.err.println("Nose ha encontrado al actor");
+		}}
 	
 	@Override
+	@Transactional
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicacion arrancada");
 		//Servicio srv = new Servicio(new Repositorio(new Configuracion()));
@@ -52,6 +87,7 @@ public class DemosApplication implements CommandLineRunner {
 		repo1.guardar();
 		repo2.guardar();
 		System.err.println("Valor= " + valor);
+		ejemplosDatos();
 	}
 	
 	private void ejemplosIOC() {
@@ -68,6 +104,7 @@ public class DemosApplication implements CommandLineRunner {
 	} 
 	
 	private void ejemplosPruebas() {
+	
 		var calc = new Calculadora();
 		System.err.println("Suma: " + calc.suma(2, 3));
 	}
@@ -77,5 +114,6 @@ public class DemosApplication implements CommandLineRunner {
 //			System.err.println("Aplicacion arrancadaaaaaaaaaaaaaa");
 //		};
 //	}
+	
 
 }
