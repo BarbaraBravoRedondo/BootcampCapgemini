a0ebb4b3795d51ea8f049535d54975664496a4db
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2025-03-31 12:23:43 +0200

    adding functions to demos.components

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -4,15 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
 import {  NotificationModalComponent } from './main';
 import { DemosComponent } from './demos/demos.component';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, NotificationModalComponent, DemosComponent],
+  imports: [CommonModule, RouterOutlet, NotificationModalComponent, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title : string = 'world'
+ 
 
 /*  constructor(out:LoggerService){
     out.error('Es un error');
--- demo-ng/src/app/demos/demos.component.ts
+++ demo-ng/src/app/demos/demos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, OnDestroy, OnInit, signal } from '@angular/core';
 import { NotificationService, NotificationType } from '../common-services';
 import { Unsubscribable } from 'rxjs';
 
@@ -9,7 +9,47 @@ import { Unsubscribable } from 'rxjs';
   styleUrl: './demos.component.css'
 })
 export class DemosComponent implements OnInit, OnDestroy {
-  private suscriptor: Unsubscribable | undefined;
+ constructor(public vm :NotificationService){} 
+ 
+private date= new Date('2025-03-31')
+public readonly nombre= signal<string>('mundo')
+public readonly fontSize= signal<number>(25)
+public readonly list= signal([{id:1, nombre:'Madrid'},{id:2, nombre:'Oviedo'},{id:3, nombre:'Barcelona'},{id:4, nombre:'Alicante'}])
+
+public resultado=signal<string>('')
+public visible=signal<boolean>(true)
+public invisible=computed<boolean>(()=>!this.visible())
+public readonly estetica =signal({importante:true, urgente:true,error:false})
+public readonly idProvincia=signal<number>(2)
+
+public get fecha():string{return this.date.toISOString();}
+public set fecha(value:string){
+this.date = new Date (value);
+}
+
+ 
+saluda(){
+  this.resultado.set('Hola ${this.nombre()}')
+}
+despide(){
+  this.resultado.set('Adios ${this.nombre()}')
+}
+di(algo: string){
+  this.resultado.set(`Di ${algo}`)
+}
+cambia(){
+  this.visible.update(valor=>!valor) 
+this.estetica.update(valor=>({...valor, importante: !valor.importante}));
+this.estetica.update(valor=> ({...valor, error: !valor.error}));}
+
+add(provincia:string){
+  const id=this.list()[this.list().length-1].id+1;
+  this.list.update(valor=>[...valor,{id, nombre:provincia}]);
+  this.idProvincia.set(id)
+}
+calcula(a:number, b:number){ return a+b}
+
+ private suscriptor?:Unsubscribable;
 
   ngOnInit(): void {
     this.suscriptor = this.vm.Notificacion.subscribe(n => {
@@ -25,6 +65,6 @@ export class DemosComponent implements OnInit, OnDestroy {
       }
       }
 
-  constructor(public vm: NotificationService) { }
+
 
 }
